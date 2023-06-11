31d3412312f53a5a5cba5ca3baac71e2f8923dba
anasselabbadi <113346805+anasselabbadi@users.noreply.github.com>
2023-06-11 14:28:28 +0100

    Update PXpy1.py
    some comment

--- PXpy1.py
+++ PXpy1.py
@@ -2,10 +2,11 @@ from PIL import Image
 
 def pixelate_image(image_path, pixel_size):
     with Image.open(image_path) as im:
-        # Resize the image to a smaller size
+        # Resize the image to a smaller size (step1)
         im = im.resize((im.width // pixel_size, im.height // pixel_size), Image.BOX)
-        # Resize the image back to its original size
+        # Resize the image back to its original size(step2)
         im = im.resize((im.width * pixel_size, im.height * pixel_size), Image.NEAREST)
+         # (step3
         im.save("pixelated_"+image_path)
 
 if __name__ == '__main__':
